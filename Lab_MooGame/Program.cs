c265955d85623930e8fc092f3b3e74392c8304a3
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-06-11 14:35:20 +0200

    Add project files.

---
+++ Lab_MooGame/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab_MooGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
