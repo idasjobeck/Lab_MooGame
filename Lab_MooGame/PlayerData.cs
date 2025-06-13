90922ce9325bddbd4a18e3f877dc2f4cfd1f2ca1
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-06-13 14:26:51 +0200

    Refactoring: Moved PlayerData class into its own file.

---
+++ Lab_MooGame/PlayerData.cs
@@ -0,0 +1,39 @@
+﻿namespace Lab_MooGame;
+
+class PlayerData
+{
+    public string UserName { get; private set; }
+    public int NumberOfGames { get; private set; }
+    int _totalNumberOfGuesses;
+
+
+    public PlayerData(string userName, int numberOfGuesses)
+    {
+        this.UserName = userName;
+        NumberOfGames = 1;
+        _totalNumberOfGuesses = numberOfGuesses;
+    }
+
+    public void Update(int numberOfGuesses)
+    {
+        _totalNumberOfGuesses += numberOfGuesses;
+        NumberOfGames++;
+    }
+
+    public double Average()
+    {
+        return (double)_totalNumberOfGuesses / NumberOfGames;
+    }
+
+
+    public override bool Equals(Object? player)
+    {
+        return UserName.Equals(((PlayerData?)player!).UserName);
+    }
+
+
+    public override int GetHashCode()
+    {
+        return UserName.GetHashCode();
+    }
+}
