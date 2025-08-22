57edead78a6ba94cf83b6a80b5bcf9ce474b3ce8
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-08-22 15:33:07 +0200

    Testing: Created MockRandom class implementing IRandom interface, to be used in testing of GenerateTarget method in MooTargetGenerator class.

---
+++ Lab_MooGameTests/Mocks/MockRandom.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab_MooGame.Models;
+
+namespace Lab_MooGameTests.Mocks;
+
+public class MockRandom : IRandom
+{
+    private readonly Queue<int> _predefinedValues;
+
+    public MockRandom(Queue<int> mockRandomValues)
+    {
+        _predefinedValues = mockRandomValues;
+    }
+    public int Next(int maxValue)
+    {
+        return _predefinedValues.Count > 0 ? _predefinedValues.Dequeue() : throw new InvalidOperationException("No more predefined values available.");
+    }
+}
