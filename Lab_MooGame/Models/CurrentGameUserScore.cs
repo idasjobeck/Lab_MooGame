043d3160d6f3be7a0adc28d7e886577b86ab2a2c
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-08-22 14:16:10 +0200

    Refactoring: Updated CurrentGameUserScore to have file-scoped namespace.

--- Lab_MooGame/Models/CurrentGameUserScore.cs
+++ Lab_MooGame/Models/CurrentGameUserScore.cs
@@ -4,16 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab_MooGame.Models
-{
-    public class CurrentGameUserScore
-    {
-        public string UserName { get; set; }
-        public int NumberOfGuesses { get; set; }
+namespace Lab_MooGame.Models;
 
-        public CurrentGameUserScore()
-        {
-            UserName = string.Empty;
-        }
+public class CurrentGameUserScore
+{
+    public string UserName { get; set; }
+    public int NumberOfGuesses { get; set; }
+
+    public CurrentGameUserScore()
+    {
+        UserName = string.Empty;
     }
 }
