a32c7aeb4ca81219ab1c1d9fef8fa4efd9b0579a
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-08-26 09:59:12 +0200

    Testing: Renamed Run_ShouldCompleteGameSuccessfully method into Run_ShouldCompleteGameSuccessfullyWithDefaultTargetLength. Added a Run_ShouldCompleteGameSuccessfullyWithSpecifiedTargetLength method which tests the Run method in GameController with a specified target length (instead of the default target length of 4).

--- Lab_MooGameTests/Controllers/GameControllerTests.cs
+++ Lab_MooGameTests/Controllers/GameControllerTests.cs
@@ -14,7 +14,7 @@ public class GameControllerTests
     [DataRow("TestUser,4321,1234,n")]
     [DataRow("Ida,1234,y,1234,n")]
     [DataRow("Ida,2143,1234,y,2134,1234,n")]
-    public void Run_ShouldCompleteGameSuccessfully(string userInputs)
+    public void Run_ShouldCompleteGameSuccessfullyWithDefaultTargetLength(string userInputs)
     {
         // Arrange
         var userInterface = new MockUI(userInputs);
@@ -26,6 +26,26 @@ public class GameControllerTests
         // Act
         gameController.Run();
 
+        // Assert
+        StringAssert.Contains(userInterface.Output, "Correct");
+    }
+
+    [DataTestMethod]
+    [TestCategory("Unit")]
+    [DataRow(6, "TestUser,123456,n")]
+    [DataRow(5, "TestUser,54321,12345,n")]
+    [DataRow(3, "Ida,123,y,123,n")]
+    [DataRow(3, "Ida,213,123,y,134,123,n")]
+    public void Run_ShouldCompleteGameSuccessfullyWithSpecifiedTargetLength(int targetLength, string userInputs)
+    {
+        // Arrange
+        var userInterface = new MockUI(userInputs);
+        var targetGenerator = new MockTargetGenerator(targetLength);
+        var guessingGame = new MooGame(targetGenerator);
+        var scoreboardService = new ScoreboardService(new TextFileDataStorage("test_highscores.txt"));
+        var gameController = new GameController(userInterface, guessingGame, scoreboardService);
+        // Act
+        gameController.Run();
         // Assert
         StringAssert.Contains(userInterface.Output, "Correct");
     }
