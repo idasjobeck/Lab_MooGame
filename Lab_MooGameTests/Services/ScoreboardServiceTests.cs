e95357908d850cf98ef47d269100799127b8108f
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-08-28 10:44:17 +0200

    Testing: Added UpdateScoreBoardTest and GetTopScoresTest for ScoreBoardServiceTests. Added SaveDataTest and GetDataTest for TextFileDataStorageTests. Added TestCategory attribute as 'NotImplemented' to all four methods.

---
+++ Lab_MooGameTests/Services/ScoreboardServiceTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Lab_MooGameTests.Services
+{
+    [TestClass]
+    public class ScoreboardServiceTests
+    {
+        [TestMethod]
+        [TestCategory("NotImplemented")]
+        public void UpdateScoreBoardTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [TestCategory("NotImplemented")]
+        public void GetTopScoresTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ Lab_MooGameTests/Services/TextFileDataStorageTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Lab_MooGameTests.Services
+{
+    [TestClass]
+    public class TextFileDataStorageTests
+    {
+        [TestMethod]
+        [TestCategory("NotImplemented")]
+        public void SaveDataTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [TestCategory("NotImplemented")]
+        public void GetDataTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
