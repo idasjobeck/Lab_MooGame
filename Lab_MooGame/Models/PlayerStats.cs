a1aa6a34c3b7d3928db92489795cab97f262a024
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-08-28 18:59:19 +0200

    Refactoring: Added 'private' access modifier to _totalNumberOfGuesses in PlayerStats. Added public property TotalNumberOfGuesses, with get only, in PlayerStats.

--- Lab_MooGame/Models/PlayerStats.cs
+++ Lab_MooGame/Models/PlayerStats.cs
@@ -4,7 +4,8 @@ public class PlayerStats
 {
     public string UserName { get; private set; }
     public int NumberOfGames { get; private set; }
-    int _totalNumberOfGuesses;
+    private int _totalNumberOfGuesses;
+    public int TotalNumberOfGuesses { get; }
 
     public PlayerStats(string userName, int numberOfGuesses)
     {
