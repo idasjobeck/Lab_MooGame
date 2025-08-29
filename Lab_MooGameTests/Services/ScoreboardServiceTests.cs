0a3e7bd43ba6c30b1a421224e2067dacabbeb014
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-08-29 15:46:20 +0200

    Testing: Amended the not implemented UpdateScoreBoardTest method in ScoreboardServiceTests into UpdateScoreBoard_ShouldSaveCurrentGameScoreForUser method which tests that the user's current game 'number of guesses' is saved.

--- Lab_MooGameTests/Services/ScoreboardServiceTests.cs
+++ Lab_MooGameTests/Services/ScoreboardServiceTests.cs
@@ -1,20 +1,35 @@
-﻿namespace Lab_MooGameTests.Services
-{
-    [TestClass]
-    public class ScoreboardServiceTests
-    {
-        [TestMethod]
-        [TestCategory("NotImplemented")]
-        public void UpdateScoreBoardTest()
-        {
-            Assert.Fail();
-        }
+﻿using Lab_MooGame.Models;
+using Lab_MooGame.Services;
+using Lab_MooGameTests.Mocks;
 
-        [TestMethod]
-        [TestCategory("NotImplemented")]
-        public void GetTopScoresTest()
-        {
-            Assert.Fail();
-        }
+namespace Lab_MooGameTests.Services;
+
+[TestClass]
+public class ScoreboardServiceTests
+{
+    [TestMethod]
+    [TestCategory("Unit")]
+    public void UpdateScoreBoard_ShouldSaveCurrentGameScoreForUser()
+    {
+        // Arrange
+        var dataStorage = new MockDataStorage();
+        var scoreboardService = new ScoreboardService(dataStorage);
+        var gameUserScore = new CurrentGameUserScore();
+        gameUserScore.UserName = "TestUser";
+        gameUserScore.NumberOfGuesses = 3;
+        var expectedSavedData = new List<string> { "TestUser#&#3" };
+
+        // Act
+        scoreboardService.UpdateScoreBoard(gameUserScore);
+
+        // Assert
+        CollectionAssert.AreEqual(expectedSavedData, dataStorage.GetData());
+    }
+
+    [TestMethod]
+    [TestCategory("NotImplemented")]
+    public void GetTopScoresTest()
+    {
+        Assert.Fail();
     }
 }
