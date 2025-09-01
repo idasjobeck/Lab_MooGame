f62afb19f43b2b9e5be158e664c00790868c34fa
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-09-01 11:58:57 +0200

    Testing: Wrote test method (SaveData_ShouldSaveDataToFile) for SaveData in TextFileDataStorage. Test asserts user score is written to file.

--- Lab_MooGameTests/Services/TextFileDataStorageTests.cs
+++ Lab_MooGameTests/Services/TextFileDataStorageTests.cs
@@ -1,13 +1,33 @@
-﻿namespace Lab_MooGameTests.Services
+﻿using System.IO.Abstractions.TestingHelpers;
+using Lab_MooGame.Services;
+
+namespace Lab_MooGameTests.Services
 {
     [TestClass]
     public class TextFileDataStorageTests
     {
+        private readonly MockFileSystem _mockFileSystem = new();
+
         [TestMethod]
         [TestCategory("NotImplemented")]
         public void SaveDataTest()
         {
-            Assert.Fail();
+            // Arrange
+            _mockFileSystem.AddEmptyFile("testfile.txt");
+            var textFileDataStorage = new TextFileDataStorage(_mockFileSystem, "testfile.txt");
+            var currentGameUserScore = new Lab_MooGame.Models.CurrentGameUserScore
+            {
+                UserName = "TestUser",
+                NumberOfGuesses = 5
+            };
+            var separator = "#&#";
+
+            // Act
+            textFileDataStorage.SaveData(currentGameUserScore, separator);
+
+            // Assert
+            var testfileContents = _mockFileSystem.GetFile("testfile.txt");
+            StringAssert.Contains(testfileContents.TextContents, "TestUser#&#5");
         }
 
         [TestMethod]
