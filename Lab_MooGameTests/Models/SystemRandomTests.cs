9b47aa755d9ef9020931cca8fc41f2f4a0042749
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-09-01 19:13:09 +0200

    Testing: Wrote test method (Next_ShouldGenerateNumberBetweenZeroAndMaxValue) for Next method in SystemRandom. Test asserts number is generated between zero and max value provided.

---
+++ Lab_MooGameTests/Models/SystemRandomTests.cs
@@ -0,0 +1,21 @@
+﻿using Lab_MooGame.Models;
+
+namespace Lab_MooGameTests.Models;
+[TestClass]
+public class SystemRandomTests
+{
+    [TestMethod]
+    public void Next_ShouldGenerateNumberBetweenZeroAndMaxValue()
+    {
+        // Arrange
+        var random = new SystemRandom();
+        var maxValue = 10;
+
+        // Act
+        var result = random.Next(maxValue);
+
+        // Assert
+        var isNumberWithinRange = result > 0 && result < maxValue;
+        Assert.IsTrue(isNumberWithinRange);
+    }
+}
