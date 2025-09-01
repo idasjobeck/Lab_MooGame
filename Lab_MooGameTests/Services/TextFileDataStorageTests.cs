b31e14b8cc0183e0f1f8e525a391eec3abc34bbc
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-09-01 12:10:02 +0200

    Testing: Wrote test method (GetData_ShouldGetDataFromFile) for GetData in TextFileDataStorage. Test asserts user score data is read from file.

--- Lab_MooGameTests/Services/TextFileDataStorageTests.cs
+++ Lab_MooGameTests/Services/TextFileDataStorageTests.cs
@@ -9,8 +9,8 @@ namespace Lab_MooGameTests.Services
         private readonly MockFileSystem _mockFileSystem = new();
 
         [TestMethod]
-        [TestCategory("NotImplemented")]
-        public void SaveDataTest()
+        [TestCategory("Unit")]
+        public void SaveData_ShouldSaveDataToFile()
         {
             // Arrange
             _mockFileSystem.AddEmptyFile("testfile.txt");
@@ -32,9 +32,19 @@ namespace Lab_MooGameTests.Services
 
         [TestMethod]
         [TestCategory("NotImplemented")]
-        public void GetDataTest()
+        public void GetData_ShouldGetDataFromFile()
         {
-            Assert.Fail();
+            // Arrange
+            var mockHighscores = new MockFileData("TestUser#&#5\r\nTestUser2#&#4\r\n");
+            _mockFileSystem.AddFile("testfile.txt", mockHighscores);
+            var textFileDataStorage = new TextFileDataStorage(_mockFileSystem, "testfile.txt");
+            var expectedData = new List<string> { "TestUser#&#5", "TestUser2#&#4" };
+
+            // Act
+            var actualData = textFileDataStorage.GetData();
+
+            // Assert
+            CollectionAssert.AreEqual(expectedData, actualData);
         }
     }
 }
