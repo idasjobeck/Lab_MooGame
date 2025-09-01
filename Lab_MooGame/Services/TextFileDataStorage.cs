703d57c24ef8e830b5a83f32d20d48ee4b30cf26
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-09-01 10:54:25 +0200

    Testing/Refactoring: Added IFileSystem property (_fileSystem) in TextFileDataStorage class; and updated constructors to have one allocate default System.IO implementation of FileSystem, and have one take in an IFileSystem argument so that a mock file system can be used in testing. Updated use of StreamReader and StreamWriter in the TextFileDataStorage class to use the newly created _fileSystem property.

--- Lab_MooGame/Services/TextFileDataStorage.cs
+++ Lab_MooGame/Services/TextFileDataStorage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Abstractions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,15 +12,22 @@ public class TextFileDataStorage : IDataStorage
 {
     private string _filePath;
     public string FilePath => _filePath;
+    private readonly IFileSystem _fileSystem;
 
-    public TextFileDataStorage(string filePath)
+    public TextFileDataStorage(string filePath) : this(new FileSystem(), filePath)
     {
+        _filePath = filePath;
+    }
+
+    public TextFileDataStorage(IFileSystem fileSystem, string filePath)
+    {
+        _fileSystem = fileSystem;
         _filePath = filePath;
     }
 
     public void SaveData(CurrentGameUserScore currentGameUsernameAndScore, string separator)
     {
-        using (StreamWriter streamWriter = new StreamWriter(_filePath, append: true))
+        using (var streamWriter = _fileSystem.File.AppendText(_filePath))
         {
             streamWriter.WriteLine($"{currentGameUsernameAndScore.UserName}{separator}{currentGameUsernameAndScore.NumberOfGuesses}");
         }
@@ -30,7 +38,7 @@ public class TextFileDataStorage : IDataStorage
         var data = new List<string>();
         string? lineOfTextRead;
 
-        using (StreamReader streamReader = new StreamReader(_filePath))
+        using (var streamReader = _fileSystem.File.OpenText(_filePath))
         {
             while ((lineOfTextRead = streamReader.ReadLine()) != null)
             {
