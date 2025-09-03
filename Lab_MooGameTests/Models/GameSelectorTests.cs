14c46fa66b21bf06843089c056e572c2094a92ec
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-09-03 22:46:10 +0200

    [VG} Testing: Wrote test method SelectGame_ShouldReturnTheCorrectGameSelectionForUserInput for SelectGame in GameSelector; test asserts correct game selection is returned based on user input.

---
+++ Lab_MooGameTests/Models/GameSelectorTests.cs
@@ -0,0 +1,34 @@
+﻿using Lab_MooGame;
+using Lab_MooGame.Models;
+using Lab_MooGame.UI;
+using Lab_MooGameTests.Mocks;
+
+namespace Lab_MooGameTests.Models;
+
+[TestClass]
+public class GameSelectorTests
+{
+    private Dictionary<GameSelection, GameConfig> _gameSelectionsAvailable = new ()
+    {
+        { GameSelection.MooGame, new GameConfig("Moo Game",4, 9, false) },
+        { GameSelection.Mastermind, new GameConfig("Mastermind", 4, 6, true) }
+    };
+
+    [DataTestMethod]
+    [TestCategory("Unit")]
+    [DataRow("1", GameSelection.MooGame)]
+    [DataRow("2", GameSelection.Mastermind)]
+    [DataRow("0", GameSelection.Exit)]
+    public void SelectGame_ShouldReturnTheCorrectGameSelectionForUserInput(string userInputs, GameSelection expectedGameSelection)
+    {
+        // Arrange
+        var ui = new MockUI(userInputs);
+        var gameSelector = new GameSelector(ui, _gameSelectionsAvailable);
+
+        // Act
+        var actualGameSelection = gameSelector.SelectGame();
+
+        // Assert
+        Assert.AreEqual(expectedGameSelection, actualGameSelection);
+    }
+}
