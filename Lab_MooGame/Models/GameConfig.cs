53e9c553e0661e0b84bf4e14ba7fa27db58ba7bd
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-09-03 18:05:50 +0200

    [VG] Created GameConfig class with properties for Name, TargetLength, MaxRange, AllowRepeats, and TargetGenerator. One constructor takes in all five properties, and one constructor takes in Name, TargetLength, MaxRange, AllowRepeats and allocates a new TargetGenerator based on TargetLength, MaxRange, AllowRepeats. The GameConfig class is just a data carrier, and has no methods.

---
+++ Lab_MooGame/Models/GameConfig.cs
@@ -0,0 +1,35 @@
+﻿using Lab_MooGame.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab_MooGame.Services;
+
+namespace Lab_MooGame.Models;
+public class GameConfig
+{
+    public string Name { get; }
+    public int TargetLength { get; }
+    public int MaxRange { get; }
+    public bool AllowRepeats { get; }
+    public ITargetGenerator TargetGenerator { get; }
+
+    public GameConfig(string name, int targetLength, int maxRange, bool allowRepeats, ITargetGenerator targetGenerator)
+    {
+        Name = name;
+        TargetLength = targetLength;
+        MaxRange = maxRange;
+        AllowRepeats = allowRepeats;
+        TargetGenerator = targetGenerator;
+    }
+
+    public GameConfig(string name, int targetLength, int maxRange, bool allowRepeats)
+    {
+        Name = name;
+        TargetLength = targetLength;
+        MaxRange = maxRange;
+        AllowRepeats = allowRepeats;
+        TargetGenerator = new TargetGenerator(TargetLength, MaxRange, AllowRepeats);
+    }
+}
