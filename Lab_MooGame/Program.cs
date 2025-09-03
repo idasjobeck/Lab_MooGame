17467e63f8c7501acf0a25d93bc4b6cafe3c7aab
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-09-03 10:26:08 +0200

    [VG] Added setup for Mastermind game to Main method in Program. Update to the code in the Main method is still needed to run the different games as no menu is implemented yet.

--- Lab_MooGame/Program.cs
+++ Lab_MooGame/Program.cs
@@ -13,14 +13,29 @@ public class Program
     public static void Main(string[] args)
     {
         var ui = new ConsoleUI();
-        var targetLength = 4;
-        var maxRange = 9;
-        var allowRepeats = false;
-        var targetGenerator = new TargetGenerator(targetLength, maxRange, allowRepeats);
-        var mooGame = new MooGame(targetGenerator);
-        var dataStorage = new TextFileDataStorage("moo_highscores.txt");
-        var scoreboardService = new ScoreboardService(dataStorage);
-        var gameController = new GameController(ui, mooGame, scoreboardService);
+
+        //Moo game setup
+        var mooTargetLength = 4;
+        var mooMaxRange = 9;
+        var mooAllowRepeats = false;
+        var mooTargetGenerator = new TargetGenerator(mooTargetLength, mooMaxRange, mooAllowRepeats);
+        var mooGame = new MooGame(mooTargetGenerator);
+        var mooDataStorage = new TextFileDataStorage("moo_highscores.txt");
+        var mooScoreboardService = new ScoreboardService(mooDataStorage);
+
+
+        //Mastermind setup
+        var mastermindTargetLength = 4;
+        var mastermindMaxRange = 6;
+        var mastermindAllowRepeats = true;
+        var mastermindTargetGenerator = new TargetGenerator(mastermindTargetLength, mastermindMaxRange, mastermindAllowRepeats);
+        var mastermindGame = new MastermindGame(mastermindTargetGenerator);
+        var mastermindDataStorage = new TextFileDataStorage("mastermind_highscores.txt");
+        var mastermindScoreboardService = new ScoreboardService(mastermindDataStorage);
+
+
+        //var gameController = new GameController(ui, mooGame, mooScoreboardService);
+        var gameController = new GameController(ui, mastermindGame, mastermindScoreboardService);
 
         gameController.Run();
     }
